f596e0a88fe7a5c59f19d0660c6c4cb86075bf46
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-08-31 16:59:22 -0700

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,17 @@
-import requests
+#!/usr/bin/python3
+"""Module for task 0"""
+
 
 def number_of_subscribers(subreddit):
-    """Query Reddit API and return the number of subscribers."""
-    # Set the User-Agent header to avoid 'Too Many Requests' error
-    headers = {'User-Agent': 'MyRedditApp/0.1'}
-    
-    # Define the URL for the subreddit's about.json endpoint
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    try:
-        # Make a GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # If the status code is 200, we assume the subreddit is valid
-        if response.status_code == 200:
-            data = response.json()
-            return data.get("data", {}).get("subscribers", 0)
-        
-        # If the status code is not 200, return 0 (invalid subreddit)
-        else:
-            return 0
-    except requests.RequestException:
-        # If there was a network-related error, return 0
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
+    return sub_info.json().get("data").get("subscribers")
