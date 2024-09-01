62f9276733a55f744ddb003d6badfa4ac05dfc76
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-08-31 17:46:28 -0700

    s

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+""" Return number of All subscribers"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit to query.
-        
-    Returns:
-        int: The number of subscribers to the subreddit, or 0 if
-             the subreddit is invalid.
-    """
+    """Get All subscribers number for a given subreddit"""
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My-User-Agent"}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'AyaTarek95'}
+    res = requests.get(url, headers=headers)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json().get("data")
-            if data and "subscribers" in data:
-                return data["subscribers"]
-        return 0
-    except requests.RequestException:
+    if res.status_code == 200:
+        data = res.json()
+        subscribers_num = data['data']['subscribers']
+        return subscribers_num
+
+    else:
         return 0
