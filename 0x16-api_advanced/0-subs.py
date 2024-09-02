f983d024fc1b293e53ee745f07a6e6583019d403
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-09-02 05:29:07 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
 """Module for task 0"""
 
-import requests
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    # Define the base URL for the Reddit API with the provided subreddit
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    # Define a custom User-Agent to avoid issues with rate limiting
-    headers = {'User-Agent': 'custom_agent_for_reddit_query'}
-
-    try:
-        # Send the GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # Check if the status code indicates a successful request
-        if response.status_code == 200:
-            # Parse the JSON response and extract he subscriber count
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            # Return 0 if the subreddit is invalid 
-            return 0
-    except requests.RequestException:
-        # In case of any other exceptions, return 0
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
+
+    return sub_info.json().get("data").get("subscribers")
