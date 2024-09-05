41869e5a3162bbcae8a614361bf1b82b4812f473
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-09-05 15:20:19 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API for number of subscribers in a subreddit"""
+"""Module for task 0"""
 
-import requests
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
-    to the subreddit. Returns 0 if the subreddit is invalid or if
-    any other error occurs."""
-    
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "python:subscribers_count:v1.0 (by /u/your_username)"}
+    to the subreddit"""
+    import requests
 
-    try:
-        # Make the request and do not follow redirects
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # If the status code is not 200 (success), return 0
-        if response.status_code != 200:
-            return 0
-        
-        # Extract the number of subscribers from the response JSON
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
-    
-    except requests.RequestException:
-        # Return 0 in case of any request exception (network issues, etc.)
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
+    return sub_info.json().get("data").get("subscribers")
